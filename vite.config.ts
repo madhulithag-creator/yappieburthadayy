3da8bb611b7d4282c7e513bb9960998ee99e1cf4
madhulithag-creator <madhulithag@gmail.com>
2026-01-29 22:32:08 +0530

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/yappieburthadayy/',
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'docs',
     emptyOutDir: true
   }
 })
-
